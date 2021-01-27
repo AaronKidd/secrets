94d419bf16e70da9d18dd2c637a0157b995bdcd9
AaronKidd <64435877+AaronKidd@users.noreply.github.com>
2021-01-27 20:54:00 +0000

    HASHED PASSWORDS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 port = 3000
 
 app.use(express.static("public"))
@@ -28,8 +29,6 @@ const secret = process.env.SECRET
 
 
 
-userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"]})
-
 
 
 const User = new mongoose.model("User",userSchema)
@@ -48,7 +47,7 @@ app.get("/register", (req,res)=>{
 app.post("/register", (req, res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save((err)=>{
@@ -64,7 +63,7 @@ app.post("/register", (req, res)=>{
 
 app.post("/login", (req,res)=>{
     const username= req.body.username;
-    const password= req.body.password;
+    const password= md5(req.body.password);
 
     User.findOne({email: username}, (err,result)=>
     {if (err){
